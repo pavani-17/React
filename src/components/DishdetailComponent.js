5472e14e8b19aceb6ba73a25191fca073342f591
Pavani <babburi.pavani@gmail.com>
2020-07-04 16:28:30 +0530

    Adding functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,88 +1,86 @@
-import {Card, CardImg,CardBody, CardText, CardTitle, CardImgOverlay} from 'reactstrap';
-import React, { Component } from 'react';
+import {Card, CardImg,CardBody, CardText, CardTitle} from 'reactstrap';
+import React from 'react';
 
-class Dishdetail extends Component
+
+
+
+function renderDish(dish)
 {
-
-    renderDish(dish)
+    if(dish!=null)
     {
-        if(dish!=null)
-        {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else
-        {
-            return (
-                <div></div>
-            );
-        }
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
     }
-
-    renderComments(dish)
+    else
     {
-        if(dish!=null)
-        { 
-            const a = dish.comments.map(
-                (comment) => 
-                {
-                    var p = Date(comment.date);
-                    const k = p.split(" ");
-                    console.log(k);
-                    return (
-                        <div className="container">
-                        <ul key={comment.id} className="list-unstyled">
-                            <div className="row">
-                                <p>{comment.comment}</p>
-                            </div>
-                            <div className="row">
-                                <p>--{comment.author}, {k[1]} {k[2]}, {k[3]} </p>
-                            </div>
-                        </ul>
+        return (
+            <div></div>
+        );
+    }
+}
+
+function renderComments(dish)
+{
+    if(dish!=null)
+    { 
+        const a = dish.comments.map(
+            (comment) => 
+            {
+                var p = Date(comment.date);
+                const k = p.split(" ");
+                return (
+                    <div className="container">
+                    <ul key={comment.id} className="list-unstyled">
+                        <div className="row">
+                            <p>{comment.comment}</p>
                         </div>
-                    )
-                }
-            );
-            return (
-                <div>
-                    <h4>Comments</h4>
+                        <div className="row">
+                            <p>--{comment.author}, {k[1]} {k[2]}, {k[3]} </p>
+                        </div>
+                    </ul>
+                    </div>
+                )
+            }
+        );
+        return (
+            <div>
+                <h4>Comments</h4>
+                {a}
+            </div>
+        );
+    }
+    else
+    {
+        return(
+            <div></div>
+        );
+    }
+}
+
+const Dishdetail = (props) =>
+{
+    const a = renderDish(props.selectedDish);
+    const b = renderComments(props.selectedDish)
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
                     {a}
                 </div>
-            );
-        }
-        else
-        {
-            return(
-                <div></div>
-            );
-        }
-    }
-
-    render() 
-    {
-        const a = this.renderDish(this.props.selectedDish);
-        const b = this.renderComments(this.props.selectedDish)
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {a}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {b}
-                    </div>
-
+                <div className="col-12 col-md-5 m-1">
+                    {b}
                 </div>
+
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Dishdetail;
