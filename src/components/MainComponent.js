dbcb70f4cf454c6c375bc529b320db1d902f5907
Pavani <babburi.pavani@gmail.com>
2020-07-16 04:49:21 +0530

    React redux form revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,10 +9,13 @@ import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import About from './AboutComponent';
 import {connect} from 'react-redux';
 import {addComment, fetchDishes} from '../redux/ActionCreator';
+import {actions} from 'react-redux-form';
+
 
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm : () => {dispatch(actions.reset('feedback'))}
 });
 
 const mapStateToProps = state => {
@@ -66,7 +69,7 @@ render()
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component = {() => <Menu dishes={this.props.dishes}/>} />
           <Route path='/menu/:dishId' component={DishWithId} />
-          <Route exact path='/contactus' component={Contact}/>
+    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
           <Route exact path='/aboutus' component={() => < About leaders={this.props.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
